2993882043e01e83188d249724364e9bff80642f
Logan Izenas <loganizenas@gmail.com>
2018-06-08 08:03:19 -0700

    Update

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -3,15 +3,16 @@ set -Ux XDG_CONFIG_HOME $HOME/.config
 set -Ux XDG_DATA_HOME $HOME/.local/share
 set -Ux XDG_CACHE_HOME $HOME/.cache
 
-set -Ux fish_user_paths /usr/local/opt/coreutils/libexec/gnubin /usr/local/sbin $CARGO_HOME/bin
-set -Ux EDITOR micro
-set -Ux _Z_DATA $HOME/.config/z
-set -Ux NUGET_PACKAGES $HOME/.cache/NuGet/packages
-set -Ux GNUPGHOME $HOME/.config/
-set -Ux DEFAULT_CHEAT_DIR $HOME/.config/cheat
-set -Ux CHEATCOLORS true
-
 set -Ux CARGO_HOME $HOME/.local/share/cargo
 set -Ux RUSTUP_HOME $HOME/.local/share/rustup
 
+set -Ux NUGET_PACKAGES $HOME/.cache/NuGet/packages
+set -Ux GNUPGHOME $HOME/.config/
+set -Ux DEFAULT_CHEAT_DIR $HOME/.config/cheat
+set -Ux _Z_DATA $HOME/.config/z
+
+set -Ux EDITOR micro
+set -Ux CHEATCOLORS true
+
+set -Ux fish_user_paths /usr/local/opt/coreutils/libexec/gnubin /usr/local/sbin $CARGO_HOME/bin
 set -Ux ENV_VARS_SET true
